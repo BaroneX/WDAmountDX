90523a4a4ee3319d0259224f3879cd3f5f5a973d
BLAKE <wangyuxno1@sina.com>
2014-09-10 20:25:51 +0800

    fixBug

--- 数字金额转换/数字金额转换/AppDelegate.m
+++ 数字金额转换/数字金额转换/AppDelegate.m
@@ -40,11 +40,7 @@
     
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
--(NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window{
 
-
-    return 1;
-}
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     NSLog(@"%@",NSStringFromSelector(_cmd));
